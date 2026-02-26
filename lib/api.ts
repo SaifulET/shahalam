c9f91e6bfbfb0a502fb6b9460c23afc4cb902aa3
SaifulET <si912999@gmail.com>
2026-02-27 02:46:38 +0600

    upadte

--- lib/api.ts
+++ lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api.ur-wsl.com",
 
   withCredentials: true, // send httpOnly cookie
 });
